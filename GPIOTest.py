791aa760aae62d3215f7aafc8137436ad8eebc39
Bobstin <justinakahn@gmail.com>
2016-08-28 11:47:40 -0400

    Adds printout of expected LED results

--- GPIOTest.py
+++ GPIOTest.py
@@ -5,11 +5,37 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 GPIO.setup(2, GPIO.OUT)
-GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-status = GPIO.input(2)
-GPIO.output(2, not status)
-print(not status)
+status = not GPIO.input(2)
+autoswitch=GPIO.input(4)
+onswitch=GPIO.input(17)
+GPIO.output(2, status)
+
+if status:
+	print('Auto status = Off')
+else:
+	print('Auto status = On')
+
+
+if autoswitch:
+	print('Auto LED = Off')
+else:
+	print('Auto LED = Blue')
+
+
+if not onswitch
+	print ('Status LED = Green')
+elif autoswitch:
+	if status:
+		print ('Status LED = Red')
+	else:
+		print ('Status LED = Green')
+else:
+	print ('Status LED = Red')
+
+
+
 
 #GPIO.cleanup()
