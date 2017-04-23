586ff8b91b6103715c672432da71532cab7087df
Bobstin <justinakahn@gmail.com>
2017-04-23 13:20:31 -0500

    Fixes PID calibration override utility

--- calibrations/PIDCalibrationOverride.py
+++ calibrations/PIDCalibrationOverride.py
@@ -4,33 +4,33 @@ sys.path.insert(0, os.path.abspath(".."))
 import pickle
 
 if os.path.isfile('../calibrations/PIDCalibration.pk1'):
-    with open('../calibrations/PIDCalibration.pk1','rb') as input:
-        HLTResults = pickle.load(input)
-        BLKResults = pickle.load(input)
+    with open('../calibrations/PIDCalibration.pk1','rb') as pickleInput:
+        HLTResults = pickle.load(pickleInput)
+        BLKResults = pickle.load(pickleInput)
 
-        print("HLT: Kp={:.2f}, Ki={:.8f}, Kd={:.2f}".format(HLTResults[0],HLTResults[1],HLTResults[2]))
-        print("BLK: Kp={:.2f}, Ki={:.8f}, Kd={:.2f}".format(BLKResults[0],BLKResults[1],BLKResults[2]))
+        print("HLT: Kp={:.6f}, Ki={:.12f}, Kd={:.2f}".format(HLTResults[0],HLTResults[1],HLTResults[2]))
+        print("BLK: Kp={:.6f}, Ki={:.12f}, Kd={:.2f}".format(BLKResults[0],BLKResults[1],BLKResults[2]))
 else:
     HLTResults = [0,0,0]
     BLKResults = [0,0,0]
 
-kettle = input(">>Please select which PID calibration to override, either HLT or BLK:")
-if kettle="HLT" or kettle="BLK":
-    Kp=float(input(">>Enter Kp"))
-    Ki=float(input(">>Enter Ki"))
-    Kd=float(input(">>Enter Kd"))
+kettle = input(">>Please select which PID calibration to override, either HLT or BLK: ")
+if kettle=="HLT" or kettle=="BLK":
+    Kp=float(input(">>Enter Kp: "))
+    Ki=float(input(">>Enter Ki: "))
+    Kd=float(input(">>Enter Kd: "))
 
-    if kettle="HLT": HLTResults=[Kp,Ki,Kd]
-    if kettle="BLK": BLKResults=[Kp,Ki,Kd]
+    if kettle=="HLT": HLTResults=[Kp,Ki,Kd]
+    if kettle=="BLK": BLKResults=[Kp,Ki,Kd]
 
     print("Proposed new calibrations (other kettle was pulled from pickle, or defaulted to all zeroes):")
     if HLTResults != []:
-        print("HLT: Kp={:.2f}, Ki={:.8f}, Kd={:.2f}".format(HLTResults[0],HLTResults[1],HLTResults[2]))
+        print("HLT: Kp={:.2f}, Ki={:.12f}, Kd={:.2f}".format(HLTResults[0],HLTResults[1],HLTResults[2]))
     if BLKResults != []:
-        print("BLK: Kp={:.2f}, Ki={:.8f}, Kd={:.2f}".format(BLKResults[0],BLKResults[1],BLKResults[2]))
+        print("BLK: Kp={:.2f}, Ki={:.12f}, Kd={:.2f}".format(BLKResults[0],BLKResults[1],BLKResults[2]))
 
-    confirm=input(">> Save new calibrations? (Y/N)")
-    if confirm="Y":
+    confirm=input(">> Save new calibrations? (Y/N): ")
+    if confirm=="Y":
         with open('PIDCalibration.pk1','wb') as output:
             pickle.dump(HLTResults,output,protocol = pickle.HIGHEST_PROTOCOL)
             pickle.dump(BLKResults,output,protocol = pickle.HIGHEST_PROTOCOL)
